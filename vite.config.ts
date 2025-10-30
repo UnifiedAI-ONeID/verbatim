492b7005b998c232648c44893c7edad55b63c094
simon.luke <simon.luke@impactoryinstitute.com>
2025-10-30 00:13:10 +0000

    security fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,25 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      build: {
-        commonjsOptions: {
-          include: ['node_modules', 'src']
-        }
-      },
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [
-        react({
-          jsxRuntime: 'automatic'
-        })
-      ],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.FIREBASE_API_KEY': JSON.stringify(env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(env.FIREBASE_MESSAGING_SENDER_ID),
-        'process.env.FIREBASE_APP_ID': JSON.stringify(env.FIREBASE_APP_ID)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  build: {
+    commonjsOptions: {
+      include: ['node_modules', 'src']
+    }
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [
+    react({
+      jsxRuntime: 'automatic'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
